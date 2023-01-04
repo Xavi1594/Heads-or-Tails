b321028f60bbc968b9b82b808c34bc6b1ae57e6d
Xavi1594 <xavi9415@hotmail.com>
2023-01-04 13:04:31 +0100

    done

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,10 +1,14 @@
-const botonTirarMoneda = document.getElementById('btn-moneda')
-const resultadosPosibles = ['cara', 'cruz']
+const botonTirarMoneda = document.getElementById('btn-moneda');
+const monedas = document.getElementsByClassName('moneda');
+const moneda = monedas[0];
+const resultadosPosibles = ['Cara', 'Cruz'];
 
 botonTirarMoneda.addEventListener("click", () => {
-    const indiceAleatorio = Math.floor(Math.random() * resultadosPosibles.length)
-    const resultadoAleatorio = resultadosPosibles[indiceAleatorio]
- 
- console.log(resultadoAleatorio);
+  const indiceAleatorio = Math.floor(Math.random() * resultadosPosibles.length);
+  const resultadoAleatorio = resultadosPosibles[indiceAleatorio];
+//   moneda.src = `img/${resultadoAleatorio}.jpg`;
+  moneda.classList.add('girando');
+  setTimeout(() => {
+    alert(resultadoAleatorio);
+  }, 1000);
 });
-
